9a6484101e3bcc93d0bccaf2f47b4a7409981b6e
Andreas Säilä <anbesa@utu.fi>
2020-03-29 00:10:40 +0200

    Uusi header tehty uudella tavalla mutta tällä kertaa mukana tulee koodikin

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import './index.css';
-import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Logo from '../../../public/images/investor9000.png';
 
-const Header = props => {
-    const { header } = props;
+const useStyles = makeStyles({
+    headerImage: {
+        height: '12rem',
+        margin: 'auto',
+        padding: '10px',
+        objectFit: 'scale-down',
+    },
+    header: {
+        textAlign: 'center',
+        border: '1px solid black',
+        background: 'linear-gradient(114deg, rgba(0, 0, 0, 1) 0%, rgba(45, 45, 45, 1) 100%)',
+        color: '#ffffff',
+    },
+});
+
+const Header = () => {
+    const classes = useStyles();
     return (
-        <div className="header">
-            <h2>{header}</h2>
+        <div className={classes.header}>
+            <img src={Logo} className={classes.headerImage} alt="Investor9000" />
         </div>
     );
-};
-
-Header.propTypes = {
-    header: PropTypes.string,
-};
-
-Header.defaultProps = {
-    header: 'investor',
 };
 
 export default Header;
