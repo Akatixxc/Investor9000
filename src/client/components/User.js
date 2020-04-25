94b32b6ff81745964f212fcf240179266e33e534
Akatixxc <Akatxxc@gmail.com>
2020-04-25 22:06:22 +0300

    Käyttäjän tiedot näyttävä palkki korjattu ja css siivottu.

--- src/client/components/User.js
+++ src/client/components/User.js
@@ -1,35 +1,57 @@
 import React from 'react';
 import './index.css';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { get } from '../api/apiHelper';
 import history from './history';
 
+const useStyles = makeStyles({
+    user: {
+        background: '#c0c0c0',
+        display: 'flex',
+        flexDirection: 'row',
+        borderBottom: '0.1px solid black',
+        '& p': {
+            objectFit: 'scale-down',
+            margin: '1rem auto 1rem auto',
+            justifyContent: 'center',
+            textAlign: 'center',
+            textTransform: 'uppercase',
+            fontFamily: 'Roboto',
+            fontSize: '1rem',
+            letterSpacing: '0.03rem',
+            lineHeight: '1.5',
+            fontWeight: '450',
+            color: '#1F1F1F',
+        },
+        '& button': {
+            margin: '1rem auto 1rem auto',
+        },
+    },
+});
+
 // Käyttäjän nimi, pääoma ja kehitys
 const User = ({ firstname, lastname, capital, date, percent, balance }) => {
+    const classes = useStyles();
+
     return (
-        <section>
+        <div className={classes.user}>
             <p>
                 {firstname} {lastname} <br />
                 {date}
             </p>
-            <div className="mydiv">
-                <h1>
-                    Pääoma <br />
-                    {capital} €
-                </h1>
-            </div>
-            <div className="mydiv">
-                <h1>
-                    Kehitys <br />
-                    {percent} %
-                </h1>
-            </div>
-            <div className="mydiv">
-                <h1>
-                    Saldo <br />
-                    {balance} €
-                </h1>
-            </div>
+            <p>
+                Pääoma <br />
+                {capital} €
+            </p>
+            <p>
+                Kehitys <br />
+                {percent} %
+            </p>
+            <p>
+                Saldo <br />
+                {balance} €
+            </p>
             <Button
                 variant="contained"
                 onClick={e => {
@@ -39,7 +61,7 @@ const User = ({ firstname, lastname, capital, date, percent, balance }) => {
             >
                 Logout
             </Button>
-        </section>
+        </div>
     );
 };
 
